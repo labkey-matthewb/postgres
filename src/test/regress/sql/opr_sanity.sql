49834ba0defe7b1ec0db8ae9ae30806b1aed9a50
Matthew Bellew <matthewb@labkey.com>
2015-10-30 14:12:52 -0700

    add ORDER BY to test case

--- src/test/regress/sql/opr_sanity.sql
+++ src/test/regress/sql/opr_sanity.sql
@@ -424,7 +424,8 @@ WHERE c.castmethod = 'b' AND
     NOT EXISTS (SELECT 1 FROM pg_cast k
                 WHERE k.castmethod = 'b' AND
                     k.castsource = c.casttarget AND
-                    k.casttarget = c.castsource);
+                    k.casttarget = c.castsource)
+ORDER BY 1, 2, 3, 4;
 
 -- **************** pg_operator ****************
 
